adef7dd1487b6d4476b74592ca77214f16c2918a
Madhu <68181437+joebeck20@users.noreply.github.com>
2024-01-09 15:09:07 +0530

    Update Filter_function.py

--- Assessment2/Filter_function.py
+++ Assessment2/Filter_function.py
@@ -10,8 +10,12 @@ def filter_numbers(predicate, iterable):
 def display(numbers):
     for number in numbers:
         print(number)
+        
+def main():
+    numbers = [2, 1, 4, 3, 6, 8, 5, 5, 10]
+    result = filter_numbers(is_even_number, numbers)
+    display(result)
 
-numbers = [2, 1, 4, 3, 6, 8, 5, 5, 10]
-result = filter_numbers(is_even_number, numbers)
-display(result)
+if __name__ == "__main__":
+    main()
 
