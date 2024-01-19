0d4bf87d558fb5883541802359b093de5167bd49
Madhu <68181437+joebeck20@users.noreply.github.com>
2024-01-19 09:44:35 +0530

    Assignment 1

---
+++ C# Exercises/ProductStore/ProductStore/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProductStore
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            List<Product> products = new List<Product>();
+
+            int choice;
+            do
+            {
+                Console.WriteLine("1. Add Product");
+                Console.WriteLine("2. Display Products");
+                Console.WriteLine("3. Exit");
+                Console.Write("Enter your choice: ");
+                choice = Convert.ToInt32(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        Product prod = new Product();
+                        Console.Write("Enter Product ID: ");
+                        prod.prodId = Convert.ToInt32(Console.ReadLine());
+                        Console.Write("Enter Name: ");
+                        prod.Name = Console.ReadLine();
+                        Console.Write("Enter Manufacturing Date (yyyy-mm-dd): ");
+                        prod.MfgDate = DateTime.Parse(Console.ReadLine());
+                        Console.Write("Enter Warranty Date (yyyy-mm-dd): ");
+                        prod.Warranty = DateTime.Parse(Console.ReadLine());
+                        Console.Write("Enter Price: ");
+                        prod.ProdPrice = Convert.ToDouble(Console.ReadLine());
+                        Console.Write("Enter Stock: ");
+                        prod.ProdStock = Convert.ToInt32(Console.ReadLine());
+                        Console.Write("Enter Discount (1-30): ");
+                        prod.ProdDiscount = Convert.ToSingle(Console.ReadLine());
+
+                        products.Add(prod);
+                        break;
+
+                    case 2:
+                        foreach (Product p in products)
+                        {
+                            Console.WriteLine(p.Display());
+                            Console.WriteLine("------------------------------");
+                        }
+                        break;
+
+                    case 3:
+                        Console.WriteLine("Exiting the program.");
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid choice. Please enter a valid option.");
+                        break;
+                }
+            } while (choice != 3);
+        }
+    }
+}
---
+++ C# Exercises/ProductStore/ProductStore/product.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace ProductStore
+{
+    internal class Product
+    {
+        private int m_intProdId;
+        public int prodId
+        {
+            get { return m_intProdId; }
+            set
+            {
+                if (value > 0)
+                    m_intProdId = value;
+                else
+                    throw new Exception("Invalid prodid");
+            }
+        }
+
+        private string m_strName;
+        public string Name
+        {
+            get { return m_strName; }
+            set
+            {
+                if (value.Length > 3)
+                    m_strName = value;
+                else
+                    throw new Exception("Name should be at least 3 chars");
+            }
+        }
+
+        private DateTime mfgDate;
+        public DateTime MfgDate
+        {
+            get { return mfgDate; }
+            set
+            {
+                if (value < DateTime.Today)
+                    mfgDate = value;
+                else
+                    throw new Exception("Manufacturing date should be before today");
+            }
+        }
+
+        private DateTime warranty;
+        public DateTime Warranty
+        {
+            get { return warranty; }
+            set
+            {
+                if (value < DateTime.Today)
+                    warranty = value;
+                else
+                    throw new Exception("Warranty should be before today");
+            }
+        }
+
+        private double m_dblPrice;
+
+        public double ProdPrice
+        {
+            get { return m_dblPrice; }
+            set
+            {
+                if (value > 0)
+                    m_dblPrice = value;
+                else
+                    throw new Exception("Price should be greater than 0");
+            }
+        }
+
+        private int m_dblStock;
+
+        public int ProdStock
+        {
+            get { return m_dblStock; }
+            set
+            {
+                if (value > 0)
+                    m_dblStock = value;
+                else
+                    throw new Exception("Stock should be greater than 0");
+            }
+        }
+
+        private double m_fltDiscount;
+
+        public double ProdDiscount
+        {
+            get { return m_fltDiscount; }
+            set
+            {
+                if (value >= 1 && value <= 30)
+                    m_fltDiscount = value;
+                else
+                    throw new Exception("Discount should be between 1 and 30");
+            }
+        }
+
+        public string Display()
+        {
+            double taxPrice = ProdPrice + ProdPrice * GetGST() / 100;
+            double discountPrice = taxPrice - taxPrice * ProdDiscount/100;
+            double totalPrice = discountPrice * ProdStock;
+
+            return $"Product ID: {prodId}\nName: {Name}\nManufacturing Date: {MfgDate.ToShortDateString()}\nWarranty Date: {Warranty.ToShortDateString()}\nPrice: {ProdPrice}\nStock: {ProdStock}\nGST: {GetGST()}\nDiscount: {ProdDiscount}%\nTotal Price: {totalPrice}";
+        }
+
+        private int GetGST()
+        {
+            // GST logic (5/12/18/28)
+            return 18;
+        }
+    }
+}
