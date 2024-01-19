cababc77b68e4a7af98fd7c0b54848f2cc218611
Madhu <68181437+joebeck20@users.noreply.github.com>
2024-01-19 09:53:08 +0530

    Assignment 2
    altered program.cs file

--- C# Exercises/ProductStore/ProductStore/Program.cs
+++ C# Exercises/ProductStore/ProductStore/Program.cs
@@ -9,11 +9,42 @@ namespace ProductStore
         {
             List<Product> products = new List<Product>();
 
+            int numProducts;
+            do
+            {
+                Console.Write("Enter the number of products to add: ");
+                numProducts = Convert.ToInt32(Console.ReadLine());
+
+                if (numProducts <= 0)
+                    Console.WriteLine("Please enter a valid number greater than 0.");
+            } while (numProducts <= 0);
+
+            for (int i = 0; i < numProducts; i++)
+            {
+                Product prod = new Product();
+                Console.Write("Enter Product ID: ");
+                prod.prodId = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter Name: ");
+                prod.Name = Console.ReadLine();
+                Console.Write("Enter Manufacturing Date (yyyy-mm-dd): ");
+                prod.MfgDate = DateTime.Parse(Console.ReadLine());
+                Console.Write("Enter Warranty Date (yyyy-mm-dd): ");
+                prod.Warranty = DateTime.Parse(Console.ReadLine());
+                Console.Write("Enter Price: ");
+                prod.ProdPrice = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Enter Stock: ");
+                prod.ProdStock = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter Discount (1-30): ");
+                prod.ProdDiscount = Convert.ToSingle(Console.ReadLine());
+
+                products.Add(prod);
+            }
+
             int choice;
             do
             {
-                Console.WriteLine("1. Add Product");
-                Console.WriteLine("2. Display Products");
+                Console.WriteLine("1. Display Products");
+                Console.WriteLine("2. Find Product by ID");
                 Console.WriteLine("3. Exit");
                 Console.Write("Enter your choice: ");
                 choice = Convert.ToInt32(Console.ReadLine());
@@ -21,31 +52,22 @@ namespace ProductStore
                 switch (choice)
                 {
                     case 1:
-                        Product prod = new Product();
-                        Console.Write("Enter Product ID: ");
-                        prod.prodId = Convert.ToInt32(Console.ReadLine());
-                        Console.Write("Enter Name: ");
-                        prod.Name = Console.ReadLine();
-                        Console.Write("Enter Manufacturing Date (yyyy-mm-dd): ");
-                        prod.MfgDate = DateTime.Parse(Console.ReadLine());
-                        Console.Write("Enter Warranty Date (yyyy-mm-dd): ");
-                        prod.Warranty = DateTime.Parse(Console.ReadLine());
-                        Console.Write("Enter Price: ");
-                        prod.ProdPrice = Convert.ToDouble(Console.ReadLine());
-                        Console.Write("Enter Stock: ");
-                        prod.ProdStock = Convert.ToInt32(Console.ReadLine());
-                        Console.Write("Enter Discount (1-30): ");
-                        prod.ProdDiscount = Convert.ToSingle(Console.ReadLine());
-
-                        products.Add(prod);
-                        break;
-
-                    case 2:
                         foreach (Product p in products)
                         {
                             Console.WriteLine(p.Display());
                             Console.WriteLine("------------------------------");
                         }
+                        break;
+
+                    case 2:
+                        Console.Write("Enter Product ID to find: ");
+                        int productIdToFind = Convert.ToInt32(Console.ReadLine());
+                        Product foundProduct = FindProductById(products, productIdToFind);
+
+                        if (foundProduct != null)
+                            Console.WriteLine(foundProduct.Display());
+                        else
+                            Console.WriteLine("Product not found.");
                         break;
 
                     case 3:
@@ -57,6 +79,16 @@ namespace ProductStore
                         break;
                 }
             } while (choice != 3);
+        }
+
+        private static Product FindProductById(List<Product> products, int productId)
+        {
+            foreach (Product p in products)
+            {
+                if (p.prodId == productId)
+                    return p;
+            }
+            return null;
         }
     }
 }
