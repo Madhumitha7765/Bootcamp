aee6cff3a080474fd2d5b0d3537e14680d3646ba
Madhu <68181437+joebeck20@users.noreply.github.com>
2024-01-22 11:50:21 +0530

    update with error message override

---
+++ CSharp Assessment/CustomAttributes/ValidateAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assessment_Device
+{
+
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = false)]
+    public abstract class ValidateAttribute : Attribute
+    {
+        public string ErrorMessage { get; set; }
+
+        public abstract string GetErrorMessage();
+    }
+}
