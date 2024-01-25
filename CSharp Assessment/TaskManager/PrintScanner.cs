40ffc3d8c0919b4c683bee76297d22695f6e8c29
Madhu <68181437+joebeck20@users.noreply.github.com>
2024-01-25 16:22:04 +0530

    Update PrintScanner.cs to eliminate redudancy

--- CSharp Assessment/TaskManager/PrintScanner.cs
+++ CSharp Assessment/TaskManager/PrintScanner.cs
@@ -7,16 +7,27 @@ using System.Threading.Tasks;
 
 namespace TaskManagerExercise
 {
-    public class PrintScanner : IPrintable, IScannable
-    {
-        public void Print(string path)
-        {
-            System.Console.WriteLine($"Printing  .....{path}");
-        }
+    
+public class PrintScanner : IPrintable, IScannable
+{
+    private readonly IPrinter printer;
+    private readonly IScanner scanner;
 
-        public void Scan(string path)
-        {
-            System.Console.WriteLine($"Scanning  .....{path}");
-        }
+    public PrintScanner()
+    {
+        this.printer = new Printer();
+        this.scanner = new Scanner();
     }
+
+    public void Print(string document)
+    {
+        this.printer.print()
+    }
+
+    public void Scan(string document)
+    {
+        this.scanner.scan()
+    }
+}
+    
 }
