aab5a115680bb1332504341fb626b59ae6a9d7dc
Madhu <68181437+Madhumitha7765@users.noreply.github.com>
2024-02-05 15:12:27 +0530

    Add files via upload

---
+++ Testing/StringCalculator/Requirements/StringCalc.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace StringCalculatorLib
+{
+    public class StringCalculator
+    {
+        public static int Add(string numbers) =>
+            string.IsNullOrEmpty(numbers) ? 0 : GetSum(numbers);
+
+        private static int GetSum(string numbers)
+        {
+            var (delimiter, newNumbers) = GetDelimiterAndNumbers(numbers);
+
+            return ContainsAny(numbers, delimiter)
+                ? SumNumbers(newNumbers, delimiter)
+                : ParseToInt(newNumbers);
+        }
+
+        private static (string Delimiter, string NewNumbers) GetDelimiterAndNumbers(string numbers)
+        {
+            if (HasSpecificDelimiter(numbers))
+            {
+                var specificDelimiters = GetSpecificDelimiters(numbers);
+                var specificNumbers = GetSpecificNumbers(numbers);
+                return (string.Join("|", specificDelimiters.Select(Regex.Escape)), specificNumbers);
+            }
+
+            return (",|\n", numbers);
+        }
+
+        private static bool HasSpecificDelimiter(string numbers) => numbers.StartsWith("//");
+
+        private static string[] GetSpecificDelimiters(string numbers) =>
+            Regex.Matches(numbers, @"\[([^\]]+)\]")
+                .Cast<Match>()
+                .Select(m => m.Groups[1].Value)
+                .ToArray();
+
+        private static string GetSpecificNumbers(string numbers) =>
+            numbers.Substring(numbers.IndexOf('\n') + 1,
+                numbers.Length - numbers.IndexOf('\n') - 1);
+
+        private static int SumNumbers(string numbers, string delimiter) =>
+            numbers.Split(delimiter.ToCharArray()).Sum(ParseToInt);
+
+        private static int ParseToInt(string n) =>
+            IsGreaterThanThousand(Convert.ToInt32(n)) ? 0 : Convert.ToInt32(string.IsNullOrEmpty(n) ? "0" : n);
+
+        private static bool IsGreaterThanThousand(int nn) => nn > 1000;
+
+        private static bool ContainsAny(string input, string getPossibleDelimiters) =>
+            getPossibleDelimiters.ToCharArray().Any(input.Contains);
+    }
+}
+
+
+
+
+
+
---
+++ Testing/StringCalculator/Requirements/StringCalculatorAddmethodTestsuite.cs
@@ -0,0 +1,122 @@
+using Xunit;
+using StringCalculatorLib;
+
+namespace StringCalculatorLib.Test
+{
+    public class InvalidParameterException : Exception
+    {
+        public InvalidParameterException(string message) : base(message) { }
+    }
+
+    public class StringCalculatorAddMethodTestSuite
+    {
+        [Fact]
+        public void GivenEmptyString_ReturnsZero()
+        {
+            string input = "";
+            int expected = 0;
+
+            int actual = StringCalculator.Add(input);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void GivenSingleZero_ReturnsZero()
+        {
+            string input = "0";
+            int expected = 0;
+
+            int actual = StringCalculator.Add(input);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void GivenTwoNumbers_ReturnsSum()
+        {
+            string input = "1,2";
+            int expected = 3;
+
+            int actual = StringCalculator.Add(input);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void GivenNegativeNumber_ThrowsException()
+        {
+            string input = "-1,2";
+
+            Assert.Throws<ArgumentException>(() => StringCalculator.Add(input));
+        }
+
+
+        [Fact]
+        public void GivenNewLineDelimiter_ReturnsSum()
+        {
+            string input = "1\n2,3";
+            int expected = 6;
+
+            int actual = StringCalculator.Add(input);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void GivenNumberGreaterThan1000_IgnoresAndReturnsSum()
+        {
+            string input = "1,1001";
+            int expected = 1;
+
+            int actual = StringCalculator.Add(input);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void GivenCustomDelimiter_ReturnsSum()
+        {
+            string input = "//;\n1;2";
+            int expected = 3;
+
+            int actual = StringCalculator.Add(input);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void GivenMultipleCharacterDelimiter_ReturnsSum()
+        {
+            string input = "//[***]\n1***2***3";
+            int expected = 6;
+
+            int actual = StringCalculator.Add(input);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void GivenMultipleDelimiters_ReturnsSum()
+        {
+            string input = "//[*][%]\n1*2%3";
+            int expected = 6;
+
+            int actual = StringCalculator.Add(input);
+
+            Assert.Equal(expected, actual);
+        }
+
+        //[Fact]
+        //public void GivenMultipleAdjacentDelimiters_ReturnsSum()
+        //{
+        //    string input = "1***3";
+        //    int expected = 4;
+
+        //    int actual = StringCalculator.Add(input);
+
+        //    Assert.Equal(expected, actual);
+        //}
+
+    }
+}
